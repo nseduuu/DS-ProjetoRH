10a8f9a3f9ae3b55077cdadf3c1aacf769d9a337
Eduardo Noronha <eduardo.noronhasampaio@gmail.com>
2023-09-04 21:23:16 -0300

    inicial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System; 
+using Aula02RH.models; 
+using Aula02RH.models.Enuns; //referenciando as listas de tipos
+
+namespace Aula02RH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            Funcionario func = new Funcionario();
+
+            Console.WriteLine("Digite o nome do funcionario: ");
+            func.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o CPF: ");
+            func.Cpf = Console.ReadLine();
+
+            Console.WriteLine("Digite a data de adimissão: ");
+            func.DataAdmissao = DateTime.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("Digite o salario: ");
+            func.Salario = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escolha o tipo de Funcinário (1 - CLT | 2 - Aprendiz) ");
+            int opcao = int.Parse(Console.ReadLine());
+
+            func.TipoFuncionario = (opcao == 1) ? TipoFuncionarioEnum.CLT : TipoFuncionarioEnum.Aprendiz;
+            //fazer a atividade a partir daqui
+            // ExibirPeriodoExperiencia, contarCaracteres, validarCpf 
+
+
+            func.ReajusteSalarial();
+            decimal valorDescontoVT = func.CalcularDescontoVT(6);
+
+            func.ExibirPeriodoExperiencia();
+            string experiencia = func.ExibirPeriodoExperiencia();
+
+            // quando o metodo estiver "private" ele é somente reconhecido dentro do mesmo arquivo, não da para referenciar em outro
+
+            Console.WriteLine("=================================");
+            Console.WriteLine($"O tempo de experiência do funcionario é: {experiencia}. \n");
+            Console.WriteLine($"O sálario reajustado é: {func.Salario}. \n");
+            Console.WriteLine($"O desconto do VT é de:  {valorDescontoVT}.");
+            Console.WriteLine("=================================");
+        }
+    }
+}
+
